ce23c0234d4fb58b0ede833c5c52581226ec069a
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-19 06:53:26 +0530

    Added

--- Progressbar - Indeterminate Mode.py
+++ Progressbar - Indeterminate Mode.py
@@ -1,1 +1,7 @@
 from tkinter import * from tkinter.ttk import *
+root = Tk()
+  
+# Progress bar widget
+progress = Progressbar(root, orient = HORIZONTAL,
+            length = 100, mode = 'indeterminate')
+  
