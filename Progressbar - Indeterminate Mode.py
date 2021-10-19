debeacad99039c6f7bc6ad510d7d3c64055dcb1f
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-19 06:56:08 +0530

    Added funtion

--- Progressbar - Indeterminate Mode.py
+++ Progressbar - Indeterminate Mode.py
@@ -5,3 +5,52 @@ root = Tk()
 progress = Progressbar(root, orient = HORIZONTAL,
             length = 100, mode = 'indeterminate')
   
+# Function
+def bar():
+    import time
+    progress['value'] = 20
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 40
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 50
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 60
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 80
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 100
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 80
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 60
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 50
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 40
+    root.update_idletasks()
+    time.sleep(0.5)
+  
+    progress['value'] = 20
+    root.update_idletasks()
+    time.sleep(0.5)
+    progress['value'] = 0
+    
+    
