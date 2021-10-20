3e10c40ebd6d22fe2df51ca25b3c1dd1e638e04d
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-20 08:36:41 +0530

    Added Functions which updated progress bar

--- Progressbar - Determinate Mode.py
+++ Progressbar - Determinate Mode.py
@@ -5,3 +5,28 @@ root = Tk()
 
 progress = Progressbar(root, orient = HORIZONTAL,
 			length = 100, mode = 'determinate')
+
+def bar():
+	import time
+	progress['value'] = 20
+	root.update_idletasks()
+	time.sleep(1)
+
+	progress['value'] = 40
+	root.update_idletasks()
+	time.sleep(1)
+
+	progress['value'] = 50
+	root.update_idletasks()
+	time.sleep(1)
+
+	progress['value'] = 60
+	root.update_idletasks()
+	time.sleep(1)
+
+	progress['value'] = 80
+	root.update_idletasks()
+	time.sleep(1)
+	progress['value'] = 100
+
+progress.pack(pady = 10)
